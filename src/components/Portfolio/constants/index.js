ac0cb416404a4e591e9d76c1298fcd37d450d9b3
Ellina S <ellina.ibr@gmail.com>
2024-02-18 23:24:51 -0600

    Minified images and added sources for projects

--- src/components/Portfolio/constants/index.js
+++ src/components/Portfolio/constants/index.js
@@ -10,7 +10,7 @@ export const LIST_2020 = [
       'The project was completed as part of the HTML Academy course. Contains main page, collection page and order form. All scripts including form validation and a slider made in pure JS. Utilizing SVG sprites and the Yandex.Maps API.',
     img: Mishka,
     withDemo: true,
-    src: '',
+    src: 'https://github.com/EllinaSo/EllinaSo.github.io/tree/main/public/portfolio/mishka',
   },
   {
     title: 'dent',
@@ -18,7 +18,7 @@ export const LIST_2020 = [
       'The project is the main page for a local dental clinic. It was commissioned by the layout designer to be used in a presentation. Made with Bootstrap, jQuery and Swiper slider.',
     img: Dent,
     withDemo: true,
-    src: '',
+    src: 'https://github.com/EllinaSo/EllinaSo.github.io/tree/main/public/portfolio/dent',
   },
   {
     title: 'surfclub',
@@ -26,13 +26,13 @@ export const LIST_2020 = [
 
     img: SurfClub,
     withDemo: true,
-    src: '',
+    src: 'https://github.com/EllinaSo/EllinaSo.github.io/tree/main/public/portfolio/surfclub',
   },
   {
     title: 'ceramico',
     description: 'Landing page for small business with navigation and products loading made with jQuery.',
     img: Ceramico,
     withDemo: true,
-    src: '',
+    src: 'https://github.com/EllinaSo/EllinaSo.github.io/tree/main/public/portfolio/ceramico',
   },
 ];
